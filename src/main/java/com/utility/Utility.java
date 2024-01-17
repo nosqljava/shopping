a0a63a84ca89bfa82858ae6c8ee35c5799fc342e
nosqljava <nosqljava@gmail.com>
2024-01-17 16:41:12 +0900

    review 수정

--- src/main/java/com/utility/Utility.java
+++ src/main/java/com/utility/Utility.java
@@ -246,5 +246,57 @@ public class Utility {
 		    return str.toString();
 		 
 		  }
+	
+	/**
+	 * reveiw paging
+	 * @param total
+	 * @param nowPage
+	 * @param recordPerPage
+	 * @param col
+	 * @param word
+	 * @param nPage
+	 * @param url
+	 * @return
+	 */
+	public static String paging(int total, int nowPage, int recordPerPage, String col, String word, 
+			int nPage, String url) {
+		int pagePerBlock = 5; // 블럭당 페이지 수
+		int totalPage = (int) (Math.ceil((double) total / recordPerPage)); // 전체 페이지
+		int totalGrp = (int) (Math.ceil((double) totalPage / pagePerBlock));// 전체 그룹
+		int nowGrp = (int) (Math.ceil((double) nPage / pagePerBlock)); // 현재 그룹
+		int startPage = ((nowGrp - 1) * pagePerBlock) + 1; // 특정 그룹의 페이지 목록 시작
+		int endPage = (nowGrp * pagePerBlock); // 특정 그룹의 페이지 목록 종료
+
+		StringBuffer str = new StringBuffer();
+		str.append("<ul class='pagination justify-content-center'> ");
+		int _nowPage = (nowGrp - 1) * pagePerBlock; // 10개 이전 페이지로 이동
+		if (nowGrp >= 2) {
+			str.append("<li class='page-item'><a class='page-link' href='" + url + "?col=" + col + "&word=" + word
+					+ "&nowPage=" + nowPage + "&npage=" + _nowPage + "'>이전</A></li>");
+		}
+
+		for (int i = startPage; i <= endPage; i++) {
+			if (i > totalPage) {
+				break;
+			}
+
+			if (nPage == i) {
+				str.append("<li class='page-item active'><a class='page-link' href='#section1'>" + i + "</a></li>");
+			} else {
+				str.append("<li class='page-item'><a class='page-link' href='" + url + "?col=" + col + "&word=" + word
+						+ "&nowPage=" + nowPage + "&nPage=" + i +  "'>" + i + "</A></li>");
+			}
+		}
+
+		_nowPage = (nowGrp * pagePerBlock) + 1; // 10개 다음 페이지로 이동
+		if (nowGrp < totalGrp) {
+			str.append("<li class='page-item'><a class='page-link' href='" + url + "?col=" + col + "&word=" + word
+					+ "&nowPage=" + nowPage + "&npage=" + _nowPage + "'>다음</A></li>");
+		}
+		str.append("</ul>");
+		str.append("</div>");
+
+		return str.toString();
+	}
 
 } //class end

b64ef1aeea872f392fb706cf88cbc9d01f501110
nosqljava <nosqljava@gmail.com>
2024-01-18 17:35:31 +0900

    연습

--- src/main/java/com/WebMvcConfiguration.java
+++ src/main/java/com/WebMvcConfiguration.java
@@ -16,12 +16,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	}
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOriginPatterns("*") // React 앱의 URL, 2.4 부터 allowedOriginPatterns 사용
-				.allowedMethods("GET", "POST", "PUT", "DELETE")
-				.allowedHeaders("*")
-				.allowCredentials(true);
-	}
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**").allowedOriginPatterns("*") // React 앱의 URL, 2.4 부터 allowedOriginPatterns 사용
+//				.allowedMethods("GET", "POST", "PUT", "DELETE")
+//				.allowedHeaders("*")
+//				.allowCredentials(true);
+//	}
 
 }

79a161d950ce272d9b25410756a11ba818e3377d
nosqljava <nosqljava@gmail.com>
2024-01-19 11:17:21 +0900

    cicd 수정

--- src/main/java/com/cicd/web/CICDCont.java
+++ src/main/java/com/cicd/web/CICDCont.java
@@ -34,7 +34,7 @@ public class CICDCont {
 			log.info(msg);
 		} 
 		  else if (sw == 4) {
-			msg = "/cide Github action + EC2 최종 테스트 222";
+			msg = "/cide Github action + EC2 최종 테스트 22222";
 			log.info(msg);
 		} 
 
